edb541e8a91ba840bb97db102a134cfa2e418bb2
All4Gis <franka1986@gmail.com>
2018-06-15 23:17:18 +0200

    cambios estilo

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -40,9 +40,9 @@ jQuery(document).ready(function() {
         Background slideshow
     */
     $('.coming-soon').backstretch([
-      "assets/img/backgrounds/1.jpg"
+      "assets/img/backgrounds/3.jpg"
+    , "assets/img/backgrounds/1.jpg"
     , "assets/img/backgrounds/2.jpg"
-    , "assets/img/backgrounds/3.jpg"
     ], {duration: 3000, fade: 750});
     
     $('.about-container').backstretch("assets/img/backgrounds/4.jpg");
